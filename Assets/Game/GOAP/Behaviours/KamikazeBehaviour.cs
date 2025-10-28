62afd2e37c389efbe44fa34e1c2bdc6ae21172b2
Foma Medvedev <tolikslonov5@gmail.com>
2025-10-28 17:59:32 +0500

    kamikaze enemy yay

--- Assets/Game/GOAP/Behaviours/KamikazeBehaviour.cs
+++ Assets/Game/GOAP/Behaviours/KamikazeBehaviour.cs
@@ -2,6 +2,7 @@ using CrashKonijn.Agent.Runtime;
 using CrashKonijn.Goap.Runtime;
 using Game.GOAP.Goals;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Game.GOAP.Behaviours
 {
@@ -10,6 +11,8 @@ namespace Game.GOAP.Behaviours
         private AgentBehaviour _agent;
         private GoapActionProvider _provider;
         private GoapBehaviour _goap;
+        private EnemyDistancesStats _distancesStats;
+        [SerializeField] private PlayerRig playerRig;
 
         void Awake()
         {
@@ -21,16 +24,32 @@ namespace Game.GOAP.Behaviours
             if (!this._goap) _goap = FindFirstObjectByType<GoapBehaviour>();
             if (!this._agent) _agent = this.GetComponent<AgentBehaviour>();
             if (!this._provider) _provider = this.GetComponent<GoapActionProvider>();
+            _distancesStats = GetComponent<EnemyDistancesStats>();
             if (!this._provider.AgentTypeBehaviour)
                 this._provider.AgentType = this._goap.GetAgentType("KamikazeAgent");
+            playerRig = GetComponent<PlayerRig>();
+            playerRig.Setup(G.Player.transform);
         }
 
         void Start()
         {
             Debug.Log("KamikazeBehaviour Start");
-            //this._provider.RequestGoal<PatrollingGoal>();
+            this._provider.RequestGoal<PatrollingGoal>();
             //this._provider.RequestGoal<ChasePlayerGoal>();
-            this._provider.RequestGoal<ShootingGoal>();
+        }
+
+        void Update()
+        {
+            if (Vector3.Distance(_agent.transform.position, G.Player.transform.position) < _distancesStats.VisibilityRange)
+            {
+                playerRig.UpdateRigWeights(true);
+                this._provider.RequestGoal<ShootingGoal>();
+            }
+            else
+            {
+                playerRig.UpdateRigWeights(false);
+                this._provider.RequestGoal<PatrollingGoal>();
+            }
         }
     }
 }
